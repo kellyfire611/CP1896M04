e273eddce2d0c7b14d220688636b308749908bbe
kellyfire611@gmail.com <kellyfire611@gmail.com>
2020-01-14 20:52:59 +0700

    Tích hợp thư viện Bootstrap + JQUERY

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-    echo 'Hello Github!';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trang chủ Web bán hàng</title>
+
+    <link rel="stylesheet" href="/CP1896M04/assets/vendor/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>Hello bootstrap</h1>
+
+
+    <script src="/CP1896M04/assets/vendor/jquery/jquery.min.js"></script>
+</body>
+</html>
